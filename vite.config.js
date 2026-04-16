fd7e8aeb1f6078a2ac70207ebc0e398f61456a9f
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-16 14:11:54 +0200

    change order in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: '/cakery-website/',
+  plugins: [react(), tailwindcss()],
 })
